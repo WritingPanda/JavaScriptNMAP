a383a756a479f16c06d59f717e409b634317f21c
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-01-12 12:45:58 -0600

    Fixed bugs and changed logo
    Callstack reached too quickly.

--- js/nmap.js
+++ js/nmap.js
@@ -1,10 +1,9 @@
-// Tabbing and navigation bar
 var active_tab = 1;
 
 function select_tab(tab_no) {
     if(tab_no == 1) {
         if(active_tab == 1) {
-            return false;
+            return;
         } else {
             active_tab = 1;
             document.getElementById('tab1').style.color = "#FFF";
@@ -19,7 +18,7 @@ function select_tab(tab_no) {
         }
     } else if(tab_no == 2) {
         if(active_tab == 2) {
-            return false;
+            return;
         } else {
             active_tab = 2;
             document.getElementById('tab2').style.color = "#FFF";
@@ -34,7 +33,7 @@ function select_tab(tab_no) {
         }
     } else {
         if(active_tab == 3) {
-            return false;
+            return;
         } else {
             active_tab = 3;
             document.getElementById('tab3').style.color = "#FFF";
@@ -50,72 +49,73 @@ function select_tab(tab_no) {
     }
 }
 
-// Globals
-var ip, start_port, end_port, port, ws, xhr, start_time;
 var scan_type = 1;
-var current_port = 0;
-var open_port_max = 300;
-var closed_port_max = 2000;
+var ip;
+var start_port;
+var end_port;
 var start_ip = [];
 var end_ip = [];
+var port;
+var blocked_ports = [0,1,7,9,11,13,15,17,19,20,21,22,23,25,37,42,43,53,77,79,87,95,101,102,103,104,109,110,111,113,115,117,119,123,135,139,143,179,389,465,512,513,514,515,526,530,531,532,540,556,563,587,601,636,993,995,2049,4045,6000];
+var ws;
+var xhr;
+var start_time;
+var current_port = 0;
+var current_ip = [];
+var open_port_max = 300;
+var closed_port_max = 2000;
 var ps_open_ports = [];
 var ps_closed_ports = [];
 var ps_timeout_ports = [];
-var current_ip = [];
 var ns_hosts_up = [];
 var network_address = [192,168,0,1];
-var blocked_ports = [0,1,7,9,11,13,15,17,19,20,21,22,23,25,37,42,43,53,77,79,87,95,101,102,103,104,109,110,111,113,115,117,119,123,135,139,143,179,389,465,512,513,514,515,526,530,531,532,540,556,563,587,601,636,993,995,2049,4045,6000];
 
-
-// Essential functions
 function scan_ports() {
-    scan_type = 1;
+    scan_type=1;
     ip = document.getElementById('ip').value;
     start_port = document.getElementById('start_port').value;
     end_port = document.getElementById('end_port').value;
-
     if(!(is_valid_ip(ip.split(".")) && is_valid_port(start_port) && is_valid_port(end_port) && (end_port > start_port))) {
         alert("Invalid IP and port values entered");
         return;
     }
 
-    current_port = 0;
-    ps_open_ports = [];
-    ps_closed_ports = [];
-    ps_timeout_ports = [];
+    current_port=0;
+    ps_open_ports=[];
+    ps_closed_ports=[];
+    ps_timeout_ports=[];
 
     reset_scan_out();
 
     document.getElementById('log').innerHTML += "----------------<br><b>Scan Log:</b><br>";
-
     if(document.port_scan.protocol[0].checked) {
-        setTimeout(scan_ports_xhr(),1);
+        setTimeout("scan_ports_xhr()",1);
     } else {
-        setTimeout(scan_ports_ws(),1);
+        setTimeout("scan_ports_ws()",1);
     }
 }
 
 function scan_ports_ws() {
     if(init_port_ps()) {
-        return false;
+        return;
     }
+
     if(is_blocked(current_port)) {
         log(current_port + "  - blocked port");
-        setTimeout(scan_ports_ws(),1);
-        return false;
+        setTimeout("scan_ports_ws()",1);
+        return;
     }
 
     start_time = (new Date).getTime();
-
     try
     {
         ws = new WebSocket("ws://" + ip + ":" + current_port);
-        setTimeout(check_ps_ws(),5);
+        setTimeout("check_ps_ws()",5);
     }
     catch(err)
     {
         document.getElementById('result').innerHTML += "<b>Scan stopped. Exception: " + err + "</b>";
-        return false;
+        return;
     }
 }
 
@@ -126,9 +126,9 @@ function check_ps_ws() {
         if(interval > closed_port_max) {
             log(current_port + " - time exceeded");
             ps_timeout_ports.push(current_port);
-            setTimeout(scan_ports_ws(),1);
+            setTimeout("scan_ports_ws()",1);
         } else {
-            setTimeout(check_ps_ws(),5);
+            setTimeout("check_ps_ws()",5);
         }
     } else {
         if(interval < open_port_max) {
@@ -138,19 +138,20 @@ function check_ps_ws() {
             log(current_port + " - closed");
             ps_closed_ports.push(current_port);
         }
-        setTimeout(scan_ports_ws(),1);
+
+        setTimeout("scan_ports_ws()",1);
     }
 }
 
 function scan_ports_xhr() {
     if(init_port_ps()) {
-        return false;
+        return;
     }
 
     if(is_blocked(current_port)) {
         log(current_port + "  - blocked port");
-        setTimeout(scan_ports_xhr(),1);
-        return false;
+        setTimeout("scan_ports_xhr()",1);
+        return;
     }
 
     start_time = (new Date).getTime();
@@ -160,25 +161,24 @@ function scan_ports_xhr() {
         xhr = new XMLHttpRequest();
         xhr.open('GET', "http://" + ip + ":" + current_port);
         xhr.send();
-        setTimeout(check_ps_xhr(),5);
+        setTimeout("check_ps_xhr()",5);
     }
     catch(err)
     {
         document.getElementById('result').innerHTML += "<b>Scan stopped. Exception: " + err + "</b>";
-        return false;
+        return;
     }
 }
 
 function check_ps_xhr() {
     var interval = (new Date).getTime() - start_time;
-
     if(xhr.readyState == 1) {
         if(interval > closed_port_max) {
             log(current_port + " - time exceeded");
             ps_timeout_ports.push(current_port);
-            setTimeout(scan_ports_xhr(),1);
+            setTimeout("scan_ports_xhr()",1);
         } else {
-            setTimeout(check_ps_xhr(),5);
+            setTimeout("check_ps_xhr()",5);
         }
     } else {
         if(interval < open_port_max) {
@@ -188,7 +188,8 @@ function check_ps_xhr() {
             log(current_port + " - closed");
             ps_closed_ports.push(current_port);
         }
-        setTimeout(scan_ports_xhr(),1);
+
+        setTimeout("scan_ports_xhr()",1);
     }
 }
 
@@ -201,6 +202,7 @@ function init_port_ps() {
     } else {
         current_port++;
     }
+
     return false;
 }
 
@@ -215,30 +217,28 @@ function scan_network() {
     start_ip = document.getElementById('start_ip').value.split(".");
     end_ip = document.getElementById('end_ip').value.split(".");
     port = document.getElementById('port').value;
-
     if(!(is_valid_ip(start_ip) && is_valid_ip(end_ip) && is_valid_port(port) && (compare_ip(start_ip,end_ip) == 3))) {
         alert("Invalid IP and port values entered");
-        return false;
+        return;
     }
-
     current_ip = [];
     ns_hosts_up = [];
 
     if(is_blocked(port)) {
         document.getElementById('result').innerHTML = port + " is a blocked port";
-        return false;
+        return;
     }
 
     if(document.network_scan.protocol[0].checked) {
-        setTimeout(scan_network_xhr(),1);
+        setTimeout("scan_network_xhr()",1);
     } else {
-        setTimeout(scan_network_ws(),1);
+        setTimeout("scan_network_ws()",1);
     }
 }
 
 function scan_network_ws() {
     if(init_ip_ns()) {
-        return false;
+        return;
     }
 
     start_time = (new Date).getTime();
@@ -246,35 +246,34 @@ function scan_network_ws() {
     try
     {
         ws = new WebSocket("ws://" + current_ip.join(".") + ":" + port);
-        setTimeout(check_ns_ws(),100);
+        setTimeout("check_ns_ws()",100);
     }
     catch(err)
     {
         document.getElementById('result').innerHTML += "<b>Scan stopped. Exception: " + err + "</b>";
-        return false;
+        return;
     }
 }
 
 function check_ns_ws() {
     var interval = (new Date).getTime() - start_time;
-
     if(ws.readyState == 0) {
         if(interval > closed_port_max) {
             log(current_ip.join(".") + " - down");
-            setTimeout(scan_network_ws(),1);
+            setTimeout("scan_network_ws()",1);
         } else {
-            setTimeout(check_ns_ws(),100);
+            setTimeout("check_ns_ws()",100);
         }
     } else {
         log(current_ip.join(".") + " - up");
         ns_hosts_up.push(current_ip.join("."));
-        setTimeout(scan_network_ws(),1);
+        setTimeout("scan_network_ws()",1);
     }
 }
 
 function scan_network_xhr() {
     if(init_ip_ns()) {
-        return false;
+        return;
     }
 
     start_time = (new Date).getTime();
@@ -289,7 +288,7 @@ function scan_network_xhr() {
     catch(err)
     {
         document.getElementById('result').innerHTML += "<b>Scan stopped. Exception: " + err + "</b>";
-        return false;
+        return;
     }
 }
 
@@ -299,14 +298,14 @@ function check_ns_xhr() {
     if(xhr.readyState == 1) {
         if(interval > closed_port_max) {
             log(current_ip.join(".") + " - down");
-            setTimeout(scan_network_xhr(),1);
+            setTimeout("scan_network_xhr()",1);
         } else {
-            setTimeout(check_ns_xhr(),100);
+            setTimeout("check_ns_xhr()",100);
         }
     } else {
         log(current_ip.join(".") + " - up");
         ns_hosts_up.push(current_ip.join("."));
-        setTimeout(scan_network_xhr(),1);
+        setTimeout("scan_network_xhr()",1);
     }
 }
 
@@ -319,6 +318,7 @@ function init_ip_ns() {
     } else {
         current_ip = increment_ip(current_ip);
     }
+
     return false;
 }
 
@@ -327,7 +327,7 @@ function results_ns() {
 }
 
 function find_private_ip() {
-    scan_type = 3;
+    scan_type=3;
     network_address = [192,168,0,1];
     reset_scan_out();
     document.getElementById('result').innerHTML = "Detection started<br>";
@@ -336,7 +336,7 @@ function find_private_ip() {
 
 function find_network() {
     if(network_address[2] > 255) {
-        network_address[2] = 0;
+        network_address[2] == 0;
         document.getElementById('result').innerHTML = "The local network could not be identified...detection stopped";
     } else {
         document.getElementById('result').innerHTML += "Currently checking - " + network_address.join(".") + " ";
@@ -345,9 +345,9 @@ function find_network() {
     }
 }
 
-function find_ip() {
+function find_ip(network) {
     if(network_address[3] > 254) {
-        network_address[3] = 1;
+        network_address[3] == 1;
         document.getElementById('result').innerHTML += "<br>IP could not be found";
     } else {
         network_address[3]++;
@@ -355,49 +355,49 @@ function find_ip() {
         is_dest_up(2);
     }
 }
-
 function is_dest_up(pis_code) {
     var pis_port = 80;
     if(pis_code == 2) {
         pis_port = 30303;
     }
+
     start_time = (new Date).getTime();
+
     try
     {
         ws = new WebSocket("ws://" + network_address.join(".") + ":" + pis_port);
         if(pis_code == 2)
         {
-            setTimeout(check_idp(2), 100);
+            setTimeout("check_idp(2)",100);
         }
         else
         {
-            setTimeout(check_idp(1), 100);
+            setTimeout("check_idp(1)",100);
         }
     }
     catch(err)
     {
         document.getElementById('result').innerHTML += "<b>Scan stopped. Exception: " + err + "</b>";
-        return false;
+        return;
     }
 }
 
 function check_idp(pis_code) {
     var interval = (new Date).getTime() - start_time;
-
     if(ws.readyState == 0) {
         if(interval > closed_port_max) {
             if(pis_code == 1) {
-                setTimeout(find_network(),1);
+                setTimeout("find_network()",1);
             } else {
-                setTimeout(find_ip(),1);
+                setTimeout("find_ip()",1);
             }
         } else {
-            setTimeout("check_idp(" + pis_code + ")", 100);
+            setTimeout("check_idp(" + pis_code + ")",100);
         }
     } else {
         if(pis_code == 1) {
             document.getElementById('result').innerHTML = "Network found -- " + network_address.join(".") + "..checking for IP<br>";
-            setTimeout(find_ip(),1);
+            setTimeout("find_ip()",1);
         } else {
             document.getElementById('result').innerHTML = "Your internal IP address is " + network_address.join(".");
         }
@@ -410,6 +410,7 @@ function is_blocked(port_no) {
             return true;
         }
     }
+
     return false;
 }
 
@@ -425,23 +426,26 @@ function copy_ip(source) {
 function is_valid_ip(v_ip) {
     if(((v_ip[0] > 0) && (v_ip[0] <= 223)) &&((v_ip[1] >= 0) && (v_ip[1] <= 255)) && ((v_ip[2] >= 0) && (v_ip[2] <= 255)) && ((v_ip[3] > 0) && (v_ip[3] < 255))) {
         return true;
+    } else {
+        return false;
     }
 }
 
 function is_valid_port(v_port) {
     if(v_port > 0 && v_port < 65536) {
         return true;
+    } else {
+        return false;
     }
 }
 
 function compare_ip(a,b) {
     for(var i=0;i<4;i++) {
         var r = _compare_int(a[i],b[i]);
-
         if(r == 1) {
-            return 1; // a is greater than b
+            return 1;//a is greater than b
         } else if(r == 3) {
-            return 3; // b is greater than a
+            return 3;//b is greater than a
         }
     }
     return 2;//b is equal to a
@@ -449,11 +453,11 @@ function compare_ip(a,b) {
 
 function _compare_int(_a,_b) {
     if(_a > _b) {
-        return 1; //_a is greater than _b
+        return 1;//_a is greater than _b
     } else if(_a == _b) {
-        return 2; //_a is equal to _b
+        return 2;//_a is equal to _b
     } else {
-        return 3; //_a is lesser than _b
+        return 3;//_a is lesser than _b
     }
 }
 
@@ -465,6 +469,7 @@ function increment_ip(inc_ip) {
             inc_ip[i-1]++;
         }
     }
+
     return inc_ip;
 }
 
